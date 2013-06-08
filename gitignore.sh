0ce84973f09ed6bffca01d1d00b0e1a1174d164e
peng.gongp <peng.gongp@alibaba-inc.com>
2013-06-08 16:52:26 +0800

    login

---
+++ gitignore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "/target" > ./.gitignore
+echo "/logs" > ./.gitignore
+echo "/.settings" >> ./.gitignore
+echo "/.classpath" >> ./.gitignore
+echo "/.project" >> ./.gitignore
+echo ".gitignore" >> ./.gitignore
+echo ".DS_Store" >> ./.gitignore
