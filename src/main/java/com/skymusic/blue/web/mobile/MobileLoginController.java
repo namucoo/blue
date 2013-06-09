b002f9a01edbba48ed6cf4841e6c4ce06afd196a
peng.gongp <peng.gongp@alibaba-inc.com>
2013-06-09 21:31:02 +0800

    'add'

--- src/main/java/com/skymusic/blue/web/mobile/MobileLoginController.java
+++ src/main/java/com/skymusic/blue/web/mobile/MobileLoginController.java
@@ -1,14 +1,22 @@
 package com.skymusic.blue.web.mobile;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.jetty.util.ajax.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springside.modules.mapper.JsonMapper;
+
+import com.skymusic.blue.entity.Resource;
+import com.skymusic.blue.service.resources.ResourceService;
+import com.skymusic.blue.service.task.TaskService;
 
 /**
  * 
@@ -25,17 +33,19 @@ public class MobileLoginController {
 
 	private JsonMapper mapper = new JsonMapper();
 
-
+	@Autowired
+	private ResourceService skyResourceService;
 	
 	@RequestMapping(value = "get",method = RequestMethod.GET)
 	@ResponseBody
 	public Object login() {
-
 		//设置需要被格式化为JSON字符串的内容.
- 		Map<String, String> map = Collections.singletonMap("content", "<p>Hello World!</p>");
-
+ 		Map<String, Object> map = new HashMap<String, Object>();
+ 		map.put("123", "456");
+ 		List<Resource> list = skyResourceService.getAllResource();
+ 		map.put("res", list);
 		//渲染返回结果.
-		return mapper.toJsonP("", map);
+		return mapper.toJson(map);
 	}
 	
 	@RequestMapping(value = "login",method = RequestMethod.GET)
--- src/main/resources/sql/mysql/schema.sql
+++ src/main/resources/sql/mysql/schema.sql
@@ -1,5 +1,24 @@
 drop table if exists sky_task;
 drop table if exists sky_user;
+drop table if exists sky_mobilesession;
+drop table if exists sky_resource;
+
+create table sky_mobilesession (
+	id bigint auto_increment,
+	user_id bigint not null,
+	mobilesession varchar(32) not null,
+    primary key (id)
+) engine=InnoDB;
+
+create table sky_resource (
+	id bigint auto_increment,
+	title varchar(128) not null,
+	url varchar(255),
+	user_id bigint not null,
+	resource_type varchar(32) not null,
+	extension varchar(32) not null,
+    primary key (id)
+) engine=InnoDB;
 
 create table sky_task (
 	id bigint auto_increment,
